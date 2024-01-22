d391c140e175e1c577f17eea80fd1761df090c46
Kevinz1708 <128469729+Kevinz1708@users.noreply.github.com>
2024-01-22 14:32:20 +0100

    legenda project en categorie

--- legenda.php
+++ legenda.php
@@ -49,333 +49,340 @@
                     <tr>
                         <th scope="row">1</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Vakantiebungalow</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">2</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Vakantiebungalow</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">3</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Vakantiebungalow</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">4</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Vakantiebungalow</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">5</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Fitnessapparaat</td>
+                        <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">6</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Fitnessapparaat</td>
+                        <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">7</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Fitnessapparaat</td>
+                        <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">8</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Fitnessapparaat</td>
+                        <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">9</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>...</td>
+                        <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">10</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Celcius Huis</td>
+                        <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">11</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Diakonessenziekenhuis</td>
+                        <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">12</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Farmbotproject</td>
+                        <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">13</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Farmbotproject</td>
+                        <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">14</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Farmbotproject</td>
+                        <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">15</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Farmbotproject</td>
+                        <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">16</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Miele</td>
+                        <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">17</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Miele</td>
+                        <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">18</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>...</td>
+                        <td>Eindproject</td>
                     </tr>
                     <tr>
                         <th scope="row">19</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>...</td>
+                        <td>Eindproject</td>
                     </tr>
                     <tr>
                         <th scope="row">20</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>...</td>
+                        <td>Eindproject</td>
                     </tr>
                     <tr>
                         <th scope="row">21</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>...</td>
+                        <td>Eindproject</td>
                     </tr>
                     <tr>
                         <th scope="row">22</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>...</td>
+                        <td>Eindproject</td>
                     </tr>
                     <tr>
                         <th scope="row">23</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Starterswoning</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">24</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Starterswoning</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">25</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Starterswoning</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">26</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Starterswoning</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">27</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Starterswoning</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">28</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Starterswoning</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">29</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Starterswoning</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">30</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Starterswoning</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">31</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Starterswoning</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">32</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Insteekhaven</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">33</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Insteekhaven</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">34</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>...</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">35</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>...</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">36</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>...</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">37</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>...</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">38</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>...</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">39</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>...</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">40</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>...</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">41</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>...</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">42</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Nas @ Home</td>
+                        <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">43</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Home Assist</td>
+                        <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">44</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>PC Builder</td>
+                        <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">45</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Wifi Secure</td>
+                        <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">46</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Een klei bedrijfsnetwerk bouwen</td>
+                        <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">47</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Project Home Netwerk</td>
+                        <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">48</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Eigen Project</td>
+                        <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">49</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Droneshow</td>
+                        <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">50</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Bortholomeus Gasthuis</td>
+                        <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">51</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Zora Robots</td>
+                        <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">52</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Interactieve Plattegrond</td>
+                        <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">53</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Bureau</td>
+                        <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">54</th>
                         <td>Jacob</td>
                         <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">55</th>
                         <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>TRL</td>
+                        <td>Bonus</td>
                     </tr>
+                    <tr>
+                        <th scope="row">56</th>
+                        <td>Jacob</td>
+                        <td>TRL</td>
+                        <td>Bonus</td>
+                    </tr>
+                    
                 
 
                 </tbody>
