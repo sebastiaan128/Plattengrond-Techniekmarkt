0f6a3d8030594a5327014b143274a73402c67b13
Kevinz1708 <128469729+Kevinz1708@users.noreply.github.com>
2024-01-22 14:43:33 +0100

    lokalen

--- legenda.php
+++ legenda.php
@@ -48,336 +48,392 @@
                    
                     <tr>
                         <th scope="row">1</th>
+                        <td>B 0.50</td>
                         <td>Jacob</td>
                         <td>Vakantiebungalow</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">2</th>
+                        <td>B 0.49</td>
                         <td>Jacob</td>
                         <td>Vakantiebungalow</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">3</th>
+                        <td>B 0.48</td>
                         <td>Jacob</td>
                         <td>Vakantiebungalow</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">4</th>
+                        <td>B 0.23</td>
                         <td>Jacob</td>
                         <td>Vakantiebungalow</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">5</th>
+                        <td>B 0.26</td>
                         <td>Jacob</td>
                         <td>Fitnessapparaat</td>
                         <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">6</th>
+                        <td>B 0.50</td>
                         <td>Jacob</td>
                         <td>Fitnessapparaat</td>
                         <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">7</th>
+                        <td>B 0.49</td>
                         <td>Jacob</td>
                         <td>Fitnessapparaat</td>
                         <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">8</th>
+                        <td>B 0.48</td>
                         <td>Jacob</td>
                         <td>Fitnessapparaat</td>
                         <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">9</th>
+                        <td>B 0.26</td>
                         <td>Jacob</td>
                         <td>...</td>
                         <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">10</th>
+                        <td>B 0.07</td>
                         <td>Jacob</td>
                         <td>Celcius Huis</td>
                         <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">11</th>
+                        <td>B 0.04</td>
                         <td>Jacob</td>
                         <td>Diakonessenziekenhuis</td>
                         <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">12</th>
+                        <td>B 0.26</td>
                         <td>Jacob</td>
                         <td>Farmbotproject</td>
                         <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">13</th>
+                        <td>B 0.22</td>
                         <td>Jacob</td>
                         <td>Farmbotproject</td>
                         <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">14</th>
+                        <td>B 0.07</td>
                         <td>Jacob</td>
                         <td>Farmbotproject</td>
                         <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">15</th>
+                        <td>B 0.04</td>
                         <td>Jacob</td>
                         <td>Farmbotproject</td>
                         <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">16</th>
+                        <td>B 0.22</td>
                         <td>Jacob</td>
                         <td>Miele</td>
                         <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">17</th>
+                        <td>B 0.23</td>
                         <td>Jacob</td>
                         <td>Miele</td>
                         <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">18</th>
+                        <td>B 0.07</td>
                         <td>Jacob</td>
                         <td>...</td>
                         <td>Eindproject</td>
                     </tr>
                     <tr>
                         <th scope="row">19</th>
+                        <td>B 0.04</td>
                         <td>Jacob</td>
                         <td>...</td>
                         <td>Eindproject</td>
                     </tr>
                     <tr>
                         <th scope="row">20</th>
+                        <td>B 0.22</td>
                         <td>Jacob</td>
                         <td>...</td>
                         <td>Eindproject</td>
                     </tr>
                     <tr>
                         <th scope="row">21</th>
+                        <td>B 0.26</td>
                         <td>Jacob</td>
                         <td>...</td>
                         <td>Eindproject</td>
                     </tr>
                     <tr>
                         <th scope="row">22</th>
+                        <td>B 0.23</td>
                         <td>Jacob</td>
                         <td>...</td>
                         <td>Eindproject</td>
                     </tr>
                     <tr>
                         <th scope="row">23</th>
+                        <td>B 0.50</td>
                         <td>Jacob</td>
                         <td>Starterswoning</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">24</th>
+                        <td>B 0.49</td>
                         <td>Jacob</td>
                         <td>Starterswoning</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">25</th>
+                        <td>B 0.48</td>
                         <td>Jacob</td>
                         <td>Starterswoning</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">26</th>
+                        <td>B 0.07</td>
                         <td>Jacob</td>
                         <td>Starterswoning</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">27</th>
+                        <td>B 0.50</td>
                         <td>Jacob</td>
                         <td>Starterswoning</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">28</th>
+                        <td>B 0.49</td>
                         <td>Jacob</td>
                         <td>Starterswoning</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">29</th>
+                        <td>B 0.48</td>
                         <td>Jacob</td>
                         <td>Starterswoning</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">30</th>
+                        <td>B 0.07</td>
                         <td>Jacob</td>
                         <td>Starterswoning</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">31</th>
+                        <td>B 0.04</td>
                         <td>Jacob</td>
                         <td>Starterswoning</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">32</th>
+                        <td>B 0.07</td>
                         <td>Jacob</td>
                         <td>Insteekhaven</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">33</th>
+                        <td>B 0.04</td>
                         <td>Jacob</td>
                         <td>Insteekhaven</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">34</th>
+                        <td>B 0.04</td>
                         <td>Jacob</td>
                         <td>...</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">35</th>
+                        <td>B 0.07</td>
                         <td>Jacob</td>
                         <td>...</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">36</th>
+                        <td>B 0.22</td>
                         <td>Jacob</td>
                         <td>...</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">37</th>
+                        <td>B 0.23</td>
                         <td>Jacob</td>
                         <td>...</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">38</th>
+                        <td>B 0.26</td>
                         <td>Jacob</td>
                         <td>...</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">39</th>
+                        <td>B 0.04</td>
                         <td>Jacob</td>
                         <td>...</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">40</th>
+                        <td>B 0.07</td>
                         <td>Jacob</td>
                         <td>...</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">41</th>
+                        <td>B 0.22</td>
                         <td>Jacob</td>
                         <td>...</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">42</th>
+                        <td>B 0.23</td>
                         <td>Jacob</td>
                         <td>Nas @ Home</td>
                         <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">43</th>
+                        <td>B 0.04</td>
                         <td>Jacob</td>
                         <td>Home Assist</td>
                         <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">44</th>
+                        <td>B 0.22</td>
                         <td>Jacob</td>
                         <td>PC Builder</td>
                         <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">45</th>
+                        <td>B 0.23</td>
                         <td>Jacob</td>
                         <td>Wifi Secure</td>
                         <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">46</th>
+                        <td>B 0.26</td>
                         <td>Jacob</td>
                         <td>Een klei bedrijfsnetwerk bouwen</td>
                         <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">47</th>
+                        <td>B 0.23</td>
                         <td>Jacob</td>
                         <td>Project Home Netwerk</td>
                         <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">48</th>
+                        <td>B 0.22</td>
                         <td>Jacob</td>
                         <td>Eigen Project</td>
                         <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">49</th>
+                        <td>B 0.23</td>
                         <td>Jacob</td>
                         <td>Droneshow</td>
                         <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">50</th>
+                        <td>B 0.26</td>
                         <td>Jacob</td>
                         <td>Bortholomeus Gasthuis</td>
                         <td>Proffesioneel</td>
                     </tr>
                     <tr>
                         <th scope="row">51</th>
+                        <td>B 0.26</td>
                         <td>Jacob</td>
                         <td>Zora Robots</td>
                         <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">52</th>
+                        <td>B 0.22</td>
                         <td>Jacob</td>
                         <td>Interactieve Plattegrond</td>
                         <td>Innovatief</td>
                     </tr>
                     <tr>
                         <th scope="row">53</th>
+                        <td>B 0.04</td>
                         <td>Jacob</td>
                         <td>Bureau</td>
                         <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">54</th>
+                        <td>B 0.07</td>
                         <td>Jacob</td>
                         <td>Thornton</td>
                         <td>Creatief</td>
                     </tr>
                     <tr>
                         <th scope="row">55</th>
+                        <td>B 0.07</td>
                         <td>Jacob</td>
                         <td>TRL</td>
                         <td>Bonus</td>
                     </tr>
                     <tr>
                         <th scope="row">56</th>
+                        <td>B 0.04</td>
                         <td>Jacob</td>
                         <td>TRL</td>
                         <td>Bonus</td>
